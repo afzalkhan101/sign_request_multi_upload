85e82aebd22445162252994df0270add3ac33f1d
Afzal Khan <afzalkhan101.contact.com>
2026-01-15 10:36:11 +0600

    updated code

--- sign_multi_doc/models/sign_request.py
+++ sign_multi_doc/models/sign_request.py
@@ -5,20 +5,17 @@ import base64
 
 class SignRequest(models.Model):
     _inherit = 'sign.request'
-
     attachment_ids = fields.Many2many(
         'ir.attachment',
         string="Additional Documents to Sign",
         help="Upload extra PDF documents that should be included in this signature request (envelope)."
     )
-
     @api.model_create_multi
     def create(self, vals_list):
         records = super().create(vals_list)
         for record in records:
             record._post_process_attachments()
         return records
-
     def write(self, vals):
         res = super().write(vals)
         if 'attachment_ids' in vals:
