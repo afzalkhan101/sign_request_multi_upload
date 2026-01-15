a57edb34fbbe5492e9930d9e93a2db6a7d3b3e7b
Afzal Khan <afzalkhan101.contact.com>
2026-01-15 10:23:40 +0600

    updated code

---
+++ sign_multi_doc/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Sign Request Multi Document',
+    'version': '1.0',
+    'category': 'Sign',
+    'summary': 'Upload multiple documents in Sign Request',
+    'depends': ['base','sign'],
+    'data': [
+        'views/sign_request_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ sign_multi_doc/models/sign_request.py
@@ -0,0 +1,56 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+import base64
+
+
+class SignRequest(models.Model):
+    _inherit = 'sign.request'
+
+    attachment_ids = fields.Many2many(
+        'ir.attachment',
+        string="Additional Documents to Sign",
+        help="Upload extra PDF documents that should be included in this signature request (envelope)."
+    )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        for record in records:
+            record._post_process_attachments()
+        return records
+
+    def write(self, vals):
+        res = super().write(vals)
+        if 'attachment_ids' in vals:
+            for rec in self:
+                rec._post_process_attachments()
+        return res
+
+    def _post_process_attachments(self):
+        """Create sign.request.document for each attachment if not already linked."""
+        self.ensure_one()
+        existing_docs = self.env['sign.request.document'].search([('sign_request_id', '=', self.id)])
+        existing_attachment_ids = set(existing_docs.mapped('attachment_id.id'))
+
+        for attachment in self.attachment_ids:
+            if attachment.id in existing_attachment_ids:
+                continue
+            if not attachment.datas:
+                continue
+
+            self.env['sign.request.document'].create({
+                'name': attachment.name,
+                'datas': attachment.datas,
+                'mimetype': attachment.mimetype,
+                'attachment_id': attachment.id,
+                'sign_request_id': self.id,
+            })
+
+    def action_send_multiple_documents(self):
+        for rec in self:
+            if rec.attachment_ids:
+                rec._post_process_attachments()
+
+    def action_send(self):
+        self.action_send_multiple_documents()
+        return super().action_send()
